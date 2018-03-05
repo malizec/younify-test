9550465f052be801fa713b1fdc63879b264764dc
Nenad Cvetkovic <nenadcv@gmail.com>
2018-03-05 17:49:32 +0100

    Magento init project

---
+++ app/code/Nenad/Tools/Model/ProductsAttributeUpdate.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace Nenad\Tools\Model;
+
+use Magento\Framework\App\{ObjectManager, State};
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\{InputInterface, InputArgument, InputOption};
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ProductsAttributeUpdate extends Command
+{
+    protected $_storeManager;
+    protected $productCollection;
+
+    public function __construct(
+        State $state,
+        \Magento\Store\Model\StoreManagerInterface $storeManager,
+        \Magento\Catalog\Model\Product\Action $action
+    )
+    {
+        // We cannot use core functions (like saving a product) unless the area
+        // code is explicitly set.
+        try {
+            $state->setAreaCode('adminhtml');
+        } catch (\Magento\Framework\Exception\LocalizedException $e) {
+            // Intentionally left empty.
+        }
+        $this->objManager = \Magento\Framework\App\ObjectManager::getInstance();
+        $this->productCollection = $this->objManager->create('Magento\Catalog\Model\ResourceModel\Product\CollectionFactory');
+        $this->_storeManager = $storeManager;
+
+        $this->action = $action;
+
+        parent::__construct();
+    }
+
+
+    protected function configure()
+    {
+        $this->setName('product:update_attribute');
+        $this->setDescription('Update all products attribute');
+
+        parent::configure();
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $storeId = $this->_storeManager->getStore()->getId();
+
+        $output->writeln('<info>Importing Products...</info>');
+        $objectManager = ObjectManager::getInstance();
+
+        // $collection = $this->productCollection->create()
+        //             ->addAttributeToSelect('*')
+        //             ->load();
+        //             
+        $productCollection = $objectManager->create('Magento\Catalog\Model\ResourceModel\Product\Collection');
+        /** Apply filters here */
+
+        $productCollection->load();
+
+        foreach ($productCollection as $product) {
+            // try{
+            //     // $productItem = $objectManager->create('Magento\Catalog\Model\Product')->load($product->getId()); 
+            //     // $product->setDelivery("YES"); //$value = 1 for yes and $value = 0 for No.
+            //     // $product->setVisibility(1);
+            //     // $product->getResource()->saveAttribute($product, 'delivery');
+            //     // $product->getResource()->saveAttribute($product, 'visibility');
+            //     // $product->save();            
+            //     // $output->writeln($product->getId() . ' UPDATED');
+
+            //     $product->setDelivery("YES");
+            //     $product->getResource()->saveAttribute($product, 'delivery');
+            //     $product->setData('delivery', 'YES')->getResource()->saveAttribute($product, 'delivery');
+
+            // } catch(Exception $e){
+            //     $output->writeln($e->getMessage());
+            // }
+            
+            $output->writeln($product->getDelivery());
+        }
+
+        $id = '1'; //product id
+        $this->action->updateAttributes([$id],['delivery' => 'YES'],$storeId);
+
+
+    }
+
+}
---
+++ tasks.php
@@ -0,0 +1,13 @@
+- install last version of magento 2 with sample data +
+
+- create product attribute with attribute code 'delivery', attribute type dropdown, scope storeview, options YES/NO +
+
+- create CLI command to update this attribute for all products respecting parameter value (parameter determins if it will set attribute values to YES or NO)
+
+- deliver project (megento files and DB) using github/bitbucket
+
+- write short documentation if you think it is necessary
+
+- write down how much time did you spent and please be realistic because we will (at the begining) make our expectations from you and your contribution, if we hire you, according to your estimation for this task.
+
+- write an explanation or give the necessary documentation.
